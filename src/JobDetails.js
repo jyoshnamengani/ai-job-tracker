9e662d3c687d4d3989ce6c2f89f2d286ca7a20d2
jyoshnamengani <jyoshnamengani11@gmail.com>
2026-02-06 15:30:37 +0530

    Create JobDetails.js in src

---
+++ src/JobDetails.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+function JobDetails() {
+  return (
+    <div>
+      <h2>Job Details</h2>
+      <p>Select a job to view more information.</p>
+    </div>
+  );
+}
+
+export default JobDetails;
